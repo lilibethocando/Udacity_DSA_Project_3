f4d41266d602197242a4aa5629d380ff39e1fc73
Lilibeth Ocando <lilibethocando@gmail.com>
2022-08-19 12:16:52 -0500

    Add probem 4 and explanation 4

---
+++ Explanation_4.md
@@ -0,0 +1,6 @@
+# Problem 4: Dutch National Flag Problem
+
+For solving this issue, I decided to iterate through the list and append
+each number to a different list, then merge all the lists togethers at the end. 
+
+Time complexity of this problem is **O(n)** and space complexity is **O(n)** as well.
---
+++ Problem_4_dutch_national_flag_problem.py
@@ -0,0 +1,55 @@
+# Dutch National Flag Problem
+# Given an input array consisting on only 0, 1, and 2,
+# sort the array in a single traversal. You're not allowed
+# to use any sorting function that Python provides.
+#
+# Note: O(n) does not necessarily mean single-traversal.
+# For e.g. if you traverse the array twice, that would still be an O(n) solution,
+# but it will not count as single traversal.
+#
+# Here is some boilerplate code and test cases to start with:
+
+def sort_012(input_list):
+    """
+    Given an input array consisting on only 0, 1, and 2, sort the array in a single traversal.
+
+    Args:
+       input_list(list): List to be sorted
+    """
+    if not input_list:
+        return "Invalid list provided"
+
+    list_0 = []
+    list_1 = []
+    list_2 = []
+    final_list = []
+    for n in input_list:
+        if n == 0:
+            list_0.append(n)
+        elif n == 1:
+            list_1.append(n)
+        else:
+            list_2.append(n)
+
+    final_list += list_0
+    final_list += list_1
+    final_list += list_2
+
+    return final_list
+
+
+
+def test_function(test_case):
+    sorted_array = sort_012(test_case)
+    print(sorted_array)
+    if sorted_array == sorted(test_case):
+        print("Pass")
+    else:
+        print("Fail")
+
+
+test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
+test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
+test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+test_function([])
+test_function([0])
